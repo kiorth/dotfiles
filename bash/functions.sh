69da15c3304f9cfea487bb2cff0adfc15b5a45a0
kiorth <kaja.hiorth@gmail.com>
2026-05-04 12:45:43 +0300

    added env function

--- bash/functions.sh
+++ bash/functions.sh
@@ -8,3 +8,21 @@ nano() {
         vim "$@"
     fi
 }
+Env() {
+    ENV_DIR="$PYTHON_ENVS"
+    local envs=()
+    local i=1
+    
+    for d in "ENV_DIR"/*; do
+        if [ -f "$d/bin/activate" ]; then 
+            envs+=("$d")
+            echo "[$i] $(basename "$d")"
+            ((i++))
+        fi 
+    done
+    if [ ${#envs[@]} -eq 0 ]; then 
+        echo "No environments found"
+        return 1
+    fi 
+    source "${envs[$((choice - 1))]}/bin/activate"
+}
